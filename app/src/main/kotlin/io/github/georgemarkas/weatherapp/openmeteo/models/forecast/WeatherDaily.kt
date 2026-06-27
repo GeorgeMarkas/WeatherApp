343135d64acffb823aec1823ef1e67145cf7b31f
GeorgeMarkas <georgemarkanton@proton.me>
2026-06-27 16:28:24 +0300

    Fixed serialization error

--- app/src/main/kotlin/io/github/georgemarkas/weatherapp/openmeteo/models/forecast/WeatherDaily.kt
+++ app/src/main/kotlin/io/github/georgemarkas/weatherapp/openmeteo/models/forecast/WeatherDaily.kt
@@ -9,7 +9,7 @@ data class WeatherDaily(
     @SerialName("temperature_2m_min") val temperatureMin: List<Double?>?,
     @SerialName("relative_humidity_2m_mean") val relativeHumidityMean: List<Int?>?,
     @SerialName("cloud_cover_mean") val cloudCoverMean: List<Int?>?,
-//    @SerialName("wind_speed_10m_max") val windSpeed: List<Double?>?,
-//    @SerialName("wind_direction_10m_dominant") val windDirection: List<Double?>?,
+    @SerialName("wind_speed_10m_max") val windSpeed: List<Double?>?,
+    @SerialName("wind_direction_10m_dominant") val windDirection: List<Double?>?,
     val time: List<Long>
 )
